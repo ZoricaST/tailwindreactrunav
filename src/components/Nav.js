ced244551eee42b3c624bd2e62f6410e8ea53bad
Zorica <zoricagithub@gmail.com>
2022-01-16 19:44:42 +0100

    ok.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,13 +9,15 @@ const Nav = ({tabovi}) => {
     <div className='shadow-md w-full fixed top-0 left-0'>
         <div className='md:flex items-center justify-between bg-white py-2 md:px-10 px-7'>
              
-            <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
-                text-gray-800'>
-                    <span className='text-3xl text-indigo-600 mr-1 pt-2'>
-                    <ion-icon name="logo-ionic"></ion-icon>
+        <div className='font-bold text-2xl cursor-pointer flex items-center font-[Poppins] 
+      text-red-900'>
+
+                    <span className='text-3xl text-orange-600 mr-1 pt-2'>
+                    <ion-icon name="basketball-outline"></ion-icon>
                     </span>
-                    Designer
-            </div>
+                
+                    Zorica
+                </div>
 
             <div onClick={()=>setOpen(!open)} className='text-3xl absolute right-8 top-6 cursor-pointer md:hidden'>
       <ion-icon name={open ? 'close':'menu'}></ion-icon>
